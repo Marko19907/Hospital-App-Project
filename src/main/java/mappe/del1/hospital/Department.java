8de21367862c2dbd622435d8749d6ecb977cf76d
Marko19907 <marko.stancic23@gmail.com>
2021-02-26 15:28:17 +0100

    Implemented Department and Hospital classes, Task 3

--- src/main/java/mappe/del1/hospital/Department.java
+++ src/main/java/mappe/del1/hospital/Department.java
@@ -1,5 +1,136 @@
 package mappe.del1.hospital;
 
-public class Department
-{
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Class Department represents a single department in the hospital
+ * It is responsible for storing a list of the employees and patients in the department
+ */
+public class Department {
+    private String departmentName;
+
+    private final HashMap<String, Person> personnel;
+
+    public Department(String departmentName) {
+        if (departmentName == null) {
+            throw new IllegalArgumentException("departmentName can not be null");
+        }
+        if (departmentName.isBlank()) {
+            throw new IllegalArgumentException("departmentName can not be blank");
+        }
+        this.departmentName = departmentName;
+
+        this.personnel = new HashMap<>();
+    }
+
+    /**
+     * Sets the department name
+     * @param newDepartmentName The new name to set, can not be null or blank
+     */
+    public void setDepartmentName(String newDepartmentName) {
+        if (newDepartmentName != null) {
+            if (!newDepartmentName.isBlank()) {
+                this.departmentName = newDepartmentName;
+            }
+        }
+    }
+
+    /**
+     * Returns the departmentName
+     * @return Returns the departmentName as a String
+     */
+    public String getDepartmentName() {
+        return this.departmentName;
+    }
+
+    /**
+     * Adds a given employee to the department
+     * @param employee The employee to add, can not be null
+     *                 and can not have the same social security
+     *                 number as an already existing employee
+     */
+    public void addEmployee(Employee employee) {
+        if (employee != null) {
+            if (!this.personnel.containsKey(employee.getSocialSecurityNumber())) {
+                this.personnel.put(employee.getSocialSecurityNumber(), employee);
+            }
+        }
+    }
+
+    /**
+     * Returns a List of employees in the department
+     * @return A List of employees in the department
+     */
+    public List<Employee> getEmployees() {
+        List<Employee> toReturn = new ArrayList<>();
+
+        if (!this.personnel.isEmpty()) {
+            for (Person person : this.personnel.values()) {
+                if (person instanceof Employee) {
+                    toReturn.add((Employee) person);
+                }
+            }
+        }
+
+        return toReturn;
+    }
+
+    /**
+     * Adds a given patient to the department
+     * @param patient The patient to add, can not be null
+     *                and can not have the same social security
+     *                number as an already existing patient
+     */
+    public void addPatient(Patient patient) {
+        if (patient != null) {
+            if (!this.personnel.containsKey(patient.getSocialSecurityNumber())) {
+                this.personnel.put(patient.getSocialSecurityNumber(), patient);
+            }
+        }
+    }
+
+    /**
+     * Returns a List of patients in the department
+     * @return A List of patients in the department
+     */
+    public List<Patient> getPatients() {
+        List<Patient> toReturn = new ArrayList<>();
+
+        if (!this.personnel.isEmpty()) {
+            for (Person person : this.personnel.values()) {
+                if (person instanceof Patient) {
+                    toReturn.add((Patient) person);
+                }
+            }
+        }
+
+        return toReturn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        Department that = (Department) o;
+        return ((this.departmentName.equals(that.departmentName)) && (this.personnel.equals(that.personnel)));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.departmentName, this.personnel);
+    }
+
+    @Override
+    public String toString() {
+        return "Department name: " + this.departmentName + "\n" +
+                "Personnel: " + this.personnel + "\n";
+    }
 }
--- src/main/java/mappe/del1/hospital/Hospital.java
+++ src/main/java/mappe/del1/hospital/Hospital.java
@@ -1,5 +1,60 @@
 package mappe.del1.hospital;
 
-public class Hospital
-{
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Hospital {
+    private final String hospitalName;
+    private final HashMap<String, Department> departmentList;
+
+    public Hospital(String hospitalName) {
+        if (hospitalName == null) {
+            throw new IllegalArgumentException("hospitalName can not be null");
+        }
+        if (hospitalName.isBlank()) {
+            throw new IllegalArgumentException("hospitalName can not be blank");
+        }
+        this.hospitalName = hospitalName;
+
+        this.departmentList = new HashMap<>();
+    }
+
+    /**
+     * Returns the name of the hospital
+     * @return name of the hospital as a String
+     */
+    public String getHospitalName() {
+        return this.hospitalName;
+    }
+
+    /**
+     * Adds a given department to the department list
+     * @param department The department to add, can not be null or a duplicate
+     */
+    public void addDepartment(Department department) {
+        if (department != null) {
+            if (!this.departmentList.containsKey(department.getDepartmentName())) {
+                this.departmentList.put(department.getDepartmentName(), department);
+            }
+        }
+    }
+
+    /**
+     * Returns a List of the departments
+     * @return List of the departments
+     */
+    public List<Department> getDepartmentList() {
+        List<Department> toReturn = new ArrayList<>();
+        if (!this.departmentList.isEmpty()) {
+            toReturn.addAll(this.departmentList.values());
+        }
+        return toReturn;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospital name: " + this.hospitalName + "\n" +
+                "Department list: " + this.departmentList + "\n";
+    }
 }
