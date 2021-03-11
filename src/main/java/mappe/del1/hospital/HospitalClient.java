33e5e98c6b8d6c0df10c10ae656356e8d2305ec0
Marko19907 <marko.stancic23@gmail.com>
2021-03-11 12:26:32 +0100

    Simplified the HospitalClient class

--- src/main/java/mappe/del1/hospital/HospitalClient.java
+++ src/main/java/mappe/del1/hospital/HospitalClient.java
@@ -4,7 +4,6 @@ import mappe.del1.hospital.exception.RemoveException;
 
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
 /**
  * Class HospitalClient represents a simple client that is used to the test the program
@@ -27,36 +26,8 @@ public class HospitalClient
     public static void main(String[] args)
     {
         HospitalClient hospitalClient = new HospitalClient();
-        hospitalClient.start();
-    }
-
-    /**
-     * Main loop, loops until the user quits
-     */
-    public void start()
-    {
-        boolean running = true;
-        while (running) {
-            this.showMenu();
-
-            switch (this.getNumberInput()) {
-                case 1:
-                    this.removeExistingEmployee();
-                    break;
-
-                case 2:
-                    this.removeNonExistingPatient();
-                    break;
-
-                case 9:
-                    running = false;
-                    break;
-
-                default:
-                    System.out.println("Invalid menu choice!");
-                    break;
-            }
-        }
+        hospitalClient.removeExistingEmployee();
+        hospitalClient.removeNonExistingPatient();
     }
 
     /**
@@ -74,7 +45,7 @@ public class HospitalClient
     {
         System.out.println("Trying to remove a random existing employee from a randomly selected department . . .");
 
-        List<Department> departmentList = this.hospital.getDepartmentList();
+        List<Department> departmentList = this.getHospital().getDepartmentList();
         if (!departmentList.isEmpty()) {
             int departmentIndex = this.randomGenerator.nextInt(departmentList.size());
             Department department = departmentList.get(departmentIndex);
@@ -93,11 +64,13 @@ public class HospitalClient
                                     + employee.getFullName()
                                     + " was successfully removed from the department "
                                     + department.getDepartmentName());
+                            System.out.println();
                         }
                         catch (RemoveException e) {
                             System.out.println("Exception thrown . . .");
                             System.out.println("The existing employee was not removed");
                             e.printStackTrace();
+                            System.out.println();
                         }
                     }
                 }
@@ -105,47 +78,39 @@ public class HospitalClient
                 else {
                     System.out.println("There are no more employees in the department "
                             + department.getDepartmentName() + " . . .");
+                    System.out.println();
                 }
             }
         }
     }
 
     /**
-     * Removes a non existing patient from the first department in the hospital
+     * Removes a non existing patient from a random department in the hospital
      */
     public void removeNonExistingPatient()
     {
-        System.out.println("Trying to remove a non existing patient from the first department . . .");
+        System.out.println("Trying to remove a non existing patient from a random department . . .");
 
         Patient patient = new Patient("TestName", "TestLastName", "123456789");
-        List<Department> departmentList = this.hospital.getDepartmentList();
+        List<Department> departmentList = this.getHospital().getDepartmentList();
         if (!departmentList.isEmpty()) {
-            Department department = departmentList.get(0);
+            int departmentIndex = this.randomGenerator.nextInt(departmentList.size());
+            Department department = departmentList.get(departmentIndex);
             if (department != null) {
                 try {
                     department.remove(patient);
                     System.out.println("The exception was not thrown . . .");
                     System.out.println("Something went wrong");
+                    System.out.println();
                 }
                 catch (RemoveException e) {
                     System.out.println("Exception thrown successfully!");
-                    System.out.println("The non existing patient was not removed");
+                    System.out.println("The non existing patient was not removed from "
+                            + department.getDepartmentName());
+                    System.out.println();
                 }
             }
         }
-    }
-
-    /**
-     * Prints the menu to the console
-     */
-    private void showMenu()
-    {
-        System.out.println();
-        System.out.println("1. Remove an existing employee from the first department");
-        System.out.println("2. Remove a non existing patient from the first department");
-        System.out.println();
-        System.out.println("9. Quit");
-        System.out.println();
     }
 
     /**
@@ -155,29 +120,5 @@ public class HospitalClient
     private Hospital getHospital()
     {
         return this.hospital;
-    }
-
-    /**
-     * Returns the number input from the console
-     * @return The input from the console as an int
-     */
-    private int getNumberInput()
-    {
-        boolean running = true;
-        int toReturn = 0;
-
-        while (running) {
-            System.out.print("> ");         // print prompt
-            Scanner scanner = new Scanner(System.in);
-
-            if (scanner.hasNextInt()) {
-                toReturn = scanner.nextInt();
-                running = false;
-            }
-            else {
-                System.out.println("You must enter a number");
-            }
-        }
-        return toReturn;
     }
 }
